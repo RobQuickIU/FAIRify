fa2cbc5ff548b1736a7ea9b183c751004fdf90e8
Rob Quick <rquick@iu.edu>
2025-11-11 10:21:08 -0500

    Update create_container.py

--- fairify_py/create_container.py
+++ fairify_py/create_container.py
@@ -1,1 +1,5 @@
 # Creates Dockerfile from information in RTE and Model Cards
+
+# Get relevent cards from user input (ViT, DistilBERT, or Whisper) and either build or infer
+
+# Use card information to create a new Dockerfile
