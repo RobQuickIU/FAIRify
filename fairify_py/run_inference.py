7a13ca37e88919c16d96ff580fbce360a265239e
Rob Quick <rquick@iu.edu>
2025-11-25 11:29:56 -0500

    Update run_inference.py

--- fairify_py/run_inference.py
+++ fairify_py/run_inference.py
@@ -1,6 +1,6 @@
 # Builds a model based on the algorithm and the specified model.
 
-# Ask user if they would like to build a ViT, DistilBERT, or Whisper model.
+# Step 1. Ask the user if they would like to build a ViT, DistilBERT, or Whisper model.
 
 # Step 2. Pull the container from http://149.165.155.66/fair-containers/containers/ model building has _infer.tar in containers.
 
