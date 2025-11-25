1d173b8866313f0b524fd2516aeda12ff0b50233
Rob Quick <rquick@iu.edu>
2025-11-25 11:10:32 -0500

    Update build_model.py

--- fairify_py/build_model.py
+++ fairify_py/build_model.py
@@ -1,1 +1,7 @@
 # Builds a model based on the algorithm and the specified model.
+
+# Ask user if they would like to build a ViT, DistilBERT, or Whisper model.
+
+# Step 2. Pull the container from http://149.165.155.66/fair-containers/containers/ model building has _build.tar in containers.
+
+# Step 3. Excute the build. docker 'run whisper_build'
